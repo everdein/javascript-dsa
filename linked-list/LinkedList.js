59d9f91dc4142888e9d34bd9cd8fdd9b77fadab5
Matthew <everdein@gmail.com>
2025-03-22 14:07:39 -0400

    findKthFromEnd and additional optimizations.

--- linked-list/LinkedList.js
+++ linked-list/LinkedList.js
@@ -9,7 +9,8 @@ class LinkedList {
   constructor(value) {
     const newNode = new Node(value);
     this.head = newNode;
-    this.tail = this.head;
+    this.tail = newNode;
+    this.length = 1;
   }
 
   printList() {
@@ -21,19 +22,11 @@ class LinkedList {
   }
 
   getHead() {
-    if (this.head === null) {
-      console.log("Head: null");
-    } else {
-      console.log("Head: " + this.head.value);
-    }
+    console.log("Head:", this.head ? this.head.value : "null");
   }
 
   getTail() {
-    if (this.tail === null) {
-      console.log("Tail: null");
-    } else {
-      console.log("Tail: " + this.tail.value);
-    }
+    console.log("Tail:", this.tail ? this.tail.value : "null");
   }
 
   makeEmpty() {
@@ -50,6 +43,95 @@ class LinkedList {
     } else {
       this.tail.next = newNode;
       this.tail = newNode;
+    }
+    this.length++;
+  }
+
+  pop() {
+    if (!this.head) return undefined;
+
+    let temp = this.head;
+    let pre = this.head;
+    while (temp.next) {
+      pre = temp;
+      temp = temp.next;
+    }
+    this.tail = pre;
+    this.tail.next = null;
+    this.length--;
+
+    if (this.length === 0) {
+      this.head = null;
+      this.tail = null;
+    }
+
+    return temp;
+  }
+
+  unshift(value) {
+    const newNode = new Node(value);
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      newNode.next = this.head;
+      this.head = newNode;
+    }
+    this.length++;
+  }
+
+  shift() {
+    if (!this.head) return undefined;
+
+    let temp = this.head;
+    this.head = this.head.next;
+    temp.next = null;
+    this.length--;
+
+    if (this.length === 0) {
+      this.tail = null;
+    }
+
+    return temp;
+  }
+
+  toArray() {
+    const values = [];
+    let temp = this.head;
+    while (temp) {
+      values.push(temp.value);
+      temp = temp.next;
+    }
+    return values;
+  }
+
+  insert(index, value) {
+    if (index < 0 || index > this.length) return false;
+    if (index === 0) return this.unshift(value);
+    if (index === this.length) return this.push(value);
+
+    const newNode = new Node(value);
+    let prev = this.head;
+    for (let i = 0; i < index - 1; i++) {
+      prev = prev.next;
+    }
+    newNode.next = prev.next;
+    prev.next = newNode;
+    this.length++;
+    return true;
+  }
+
+  reverse() {
+    let temp = this.head;
+    this.head = this.tail;
+    this.tail = temp;
+    let next = temp.next;
+    let prev = null;
+    for (let i = 0; i < this.length; i++) {
+      next = temp.next;
+      temp.next = prev;
+      prev = temp;
+      temp = next;
     }
   }
 
@@ -75,11 +157,20 @@ class LinkedList {
   }
 
   findKthFromEnd(k) {
-
+    if (k < 0 || this.head === null) return null;
+    let fast = this.head;
+    let slow = this.head;
+    for (let i = 0; i < k; i++) {
+      if (fast === null) return null;
+      fast = fast.next;
+    }
+    while (fast !== null) {
+      fast = fast.next;
+      slow = slow.next;
+    }
+    return slow;
   }
-
 }
-
 
 
 let myLinkedList = new LinkedList(1);
